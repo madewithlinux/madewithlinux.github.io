4e44d77d13aa2c13c099884212fc2e335e464700
Josh Wright <madewithlinux.josh@gmail.com>
2019-12-26 12:14:10 -0600

    update blog generator

--- local-server.sh
+++ local-server.sh
@@ -5,5 +5,5 @@ trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 livereloadx -s . &
 
 while true; do
-    inotifywait -e modify,create,delete -r ./blog/ && make -C blog/ 
+    inotifywait -e modify,create,delete -r ./blog/ && echo "rebuilding" && make -C blog/
 done
